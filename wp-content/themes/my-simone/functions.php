cd521a23a29ebd8f78c2bd6b864143e14c35bebb
kweku360 <kweku360@gmail.com>
2015-02-23 12:36:55 +0000

    =more work on theme 2

--- wp-content/themes/my-simone/functions.php
+++ wp-content/themes/my-simone/functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
+	$content_width = 600; /* pixels */
 }
 
 if ( ! function_exists( 'my_simone_setup' ) ) :
@@ -38,7 +38,7 @@ function my_simone_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
@@ -46,13 +46,13 @@ function my_simone_setup() {
 	) );
 
 	// Enable support for Post Formats.
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+	add_theme_support( 'post-formats', array( 'aside') );
 
 	// Setup the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'my_simone_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+//	add_theme_support( 'custom-background', apply_filters( 'my_simone_custom_background_args', array(
+//		'default-color' => 'ffffff',
+//		'default-image' => '',
+//	) ) );
 
 	// Enable support for HTML5 markup.
 	add_theme_support( 'html5', array(
@@ -60,6 +60,7 @@ function my_simone_setup() {
 		'search-form',
 		'comment-form',
 		'gallery',
+		'caption'
 	) );
 }
 endif; // my_simone_setup
@@ -99,7 +100,7 @@ add_action( 'wp_enqueue_scripts', 'my_simone_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-//require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
