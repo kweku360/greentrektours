7c64984dbb11e7b21e2ec84181b8e89f996e8454
kweku360 <kweku360@gmail.com>
2015-02-23 16:48:41 +0000

    =more work on theme 12

--- wp-content/themes/my-simone/js/superfish-settings.js
+++ wp-content/themes/my-simone/js/superfish-settings.js
@@ -2,13 +2,28 @@
  * Created by apple on 2/23/15.
  */
 /*
- * Custom Superfish settings
+ * Custom Responsive Superfish settings
  */
 
 jQuery(document).ready(function($){
+    var breakpoint = 600;
     var sf = $('ul.nav-menu');
-    sf.superfish({
-        delay: 200,
-        speed: 'fast'
+
+    if($(document).width() >= breakpoint){
+        sf.superfish({
+            delay: 200,
+            speed: 'fast'
+        });
+    }
+
+    $(window).resize(function(){
+        if($(document).width() >= breakpoint & !sf.hasClass('sf-js-enabled')){
+            sf.superfish({
+                delay: 200,
+                speed: 'fast'
+            });
+        } else if($(document).width() < breakpoint) {
+            sf.superfish('destroy');
+        }
     });
 });
